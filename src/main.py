61e663236c9e66878c5e5df286504047f93a3c68
Kyle Stewart <4b796c65@gmail.com>
2017-03-05 23:12:11 -0800

    Tried to use ECS
    This implementation of the entity-component-system did not turn out well.

--- src/main.py
+++ src/main.py
@@ -3,10 +3,34 @@ import time
 
 import tcod
 
+import ecs
+
 #FONT = 'data/terminal8x8_gs_ro.png'
 FONT = 'data/terminal8x12_gs_ro.png'
 
 console = None
+
+class Position(ecs.Component):
+    def __init__(self, entity, x, y):
+        super().__init__(entity)
+        self.x = x
+        self.y = y
+
+class OldGraphicPosition(Position):
+    pass
+
+class Graphic(ecs.Component):
+    def __init__(self, entity, ch, fg):
+        self.ch = ch
+        self.fg = fg
+        super().__init__(entity)
+
+class RendererSystem(ecs.System):
+    def ev_added(self, component):
+        if isinstance(component, Graphic):
+            pos, = component.entity[Position]
+            console.ch[pos.y, pos.x] = component.ch
+            console.fg[pos.y, pos.x] = component.fg
 
 def main():
     global console
@@ -16,6 +40,15 @@ def main():
         16, 16
         )
     console = tcod.console_init_root(60, 40, 'Test', False)
-    console.print_(0, 0, 'Hello World!')
-    tcod.console_flush()
-    time.sleep(2)
+
+    world = ecs.World()
+    renderer_sys = RendererSystem(world)
+
+    player = world.new_entity()
+    player[Position].add(x=0, y=0)
+    player[Graphic].add(ch=ord('@'), fg=(255, 255, 255))
+
+    #console.print_(0, 0, 'Hello World!')
+    while not tcod.console_is_window_closed():
+        key = tcod.console_wait_for_keypress(False)
+        tcod.console_flush()
